8574f3dcac57b5df0cb231e0e5191b03030e4f7a
Alexander Ho <39490762+hoalexander44@users.noreply.github.com>
2020-08-26 14:02:59 -0400

    Add score display (#7)
    * save
    
    * changed algorithm and displayed score and hotspots

--- client/src/pages/hotSpotPage.js
+++ client/src/pages/hotSpotPage.js
@@ -3,13 +3,165 @@ import { Link } from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import axios from "axios";
 import LinkBar from '../components/LinkBar'
+import DocumentMeta from 'react-document-meta';
+import ButtonComponent from '../components/ButtonComponent'
 
+const meta = { name: "viewport", content: "width=device-width, initial-scale=1.0" }
 class hotSpotPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            linkBar: null,
+            locationUI: []
+        };
+    }
+
+    async componentDidMount() {
+        //console.log(this.props.location.state.username)
+        await this.setState({ username: this.props.location.state.username })
+        let table = [];
+        table.push(
+            <LinkBar key="linkBar" username={this.props.location.state.username} />
+        )
+        await this.setState({ linkBar: table })
+
+        let response = await fetch("http://localhost:5001/getLocations", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        let responseJson = await response.json();
+        console.log(responseJson);
+        this.refreshLocationUI(-1);
+    }
+
+    async getLocationInfo() {
+        let response = await fetch("http://localhost:5001/getLocations", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        let responseJson = await response.json();
+        return responseJson;
+    } 
+
+
+    async refreshLocationUI(order) {
+        let locationInfo = await this.getLocationInfo();
+        this.updateLocationUI(locationInfo.locations, order)
+    }
+
+
+    updateLocationUI(locationInfo, order) {
+        console.log(locationInfo);
+        let rawScores = [];
+        for (let x = 0; x < locationInfo.length; x++) {
+            rawScores.push(locationInfo[x].location_score);
+        }
+
+        let smallToLargeScore = this.copyArray(rawScores);
+        smallToLargeScore.sort(function (a, b) { return a - b });
+
+        let largeToSmallScore = this.copyArray(rawScores);
+        largeToSmallScore.sort(function (a, b) { return b - a });
+
+
+        let table = [];
+
+        let chosenSortedList = [];
+        if (order == -1) {
+            chosenSortedList = this.copyArray(largeToSmallScore);
+        }
+        else {
+            chosenSortedList = this.copyArray(smallToLargeScore);
+        }
+
+        console.log(chosenSortedList);
+
+
+        for (let i = 0; i < chosenSortedList.length; i++) {
+
+            let divKey = "friendStats" + i;
+            let nameKey = "friendName" + i;
+            let pointKey = "Points" + i;
+
+
+            let matchFind = this.findMatching(locationInfo, chosenSortedList[i])
+            let index = matchFind.index;
+            console.log(index);
+
+            let className = 'low';
+            if (chosenSortedList[i] > 2000 ) {
+                className = "dangerous";
+            }
+            else if (chosenSortedList[i] <= 2000 && chosenSortedList[i] > 500) {
+                className = "medium";
+            }
+            else {
+                className = "low";
+            }
+
+            //let infection = '';
+            //if (friendInfo[index].isinfected) {
+            //    infection = '\u25C8'
+            //}
+
+            table.push(
+                <div key={divKey} className={className} > <div key={nameKey}>{locationInfo[index].location_name}</div><div key={pointKey}>Points: {chosenSortedList[i]}</div></ div>
+            )
+            locationInfo.splice(index, 1);
+        }
+
+
+        this.setState({ locationUI: table });
+    }
+
+    copyArray(arrToCopy) {
+        let copyArray = [];
+        for (let i = 0; i < arrToCopy.length; i++) {
+            copyArray.push(arrToCopy[i]);
+        }
+        return copyArray;
+    }
+
+    findMatching(locationInfoList, score) {
+        console.log("SCORE: " + score);
+        let index = 0;
+        let found = false;
+        for (let i = 0; i < locationInfoList.length; i++) {
+            if (locationInfoList[i].location_score == score) {
+                index = i;
+                found = true;
+            }
+        }
+
+        return { index: index, found: found }
+    }
+
+    lowToHigh = () => {
+        this.refreshLocationUI(-1);
+    }
+
+    highToLow = () => {
+        this.refreshLocationUI(1);
+    }
+
     render() {
         return (
             <div>
-                <LinkBar />
-                <h1>HOT SPOT PAGE</h1>
+                <DocumentMeta {...meta} />
+                <div className="forceMargin">
+                {this.state.linkBar}
+                <ButtonComponent label="Best Score to Worst Score" isPressed={this.highToLow} />
+                <ButtonComponent label="Worst Score to Best Score" isPressed={this.lowToHigh} />
+                <h1>HOT SPOTS</h1>
+                    {this.state.locationUI}
+                </div>
             </div>
         );
     }
--- client/src/pages/userPage.js
+++ client/src/pages/userPage.js
@@ -7,13 +7,17 @@ import ButtonComponent from '../components/ButtonComponent'
 import TextInputComponent from '../components/TextInputComponent'
 import DocumentMeta from 'react-document-meta';
 
+let locationName = '';
 const meta = { name:"viewport", content:"width=device-width, initial-scale=1.0"}
 class userPage extends Component {
     constructor() {
         super();
         this.state = {
             username: "",
-            linkBar: null
+            linkBar: null,
+            locationsUI: null,
+            score: 0,
+            levelStyle: ''
         };
     }
 
@@ -25,21 +29,129 @@ class userPage extends Component {
             <LinkBar key="linkBar" username={this.props.location.state.username}/>
         )
         await this.setState({ linkBar: table })
+
+        let response = await fetch("http://localhost:5001/getUserData", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+            })
+        })
+
+        let responseJson = await response.json();
+        this.setState({ score: responseJson.score });
+
+
+        console.log(responseJson);
+
+        this.updateLevelStyle(responseJson.score);
+
+        let locationInfo = await this.getLocationInfo();
+        console.log(locationInfo.locations);
+
+        let UITable = [];
+        for (let i = 0; i < locationInfo.locations.length; i++) {
+            UITable.push(<p>{locationInfo.locations[i].location_name}</p>)
+        }
+        this.setState({locationsUI: UITable})
     }
+
+    updateLevelStyle(score) {
+        console.log(score)
+        if (score > 1000) {
+            this.setState({ levelStyle: "dangerousText" })
+        }
+        else if (score >= 500 && score <= 1000) {
+            this.setState({ levelStyle: "mediumText" })
+        }
+        else {
+            this.setState({ levelStyle: "lowText" })
+        }
+    }
+
+    async getLocationInfo() {
+        let response = await fetch("http://localhost:5001/getLocations", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        let responseJson = await response.json();
+        return responseJson;
+    } 
+
+
+    // keeps track of the add friend input
+    locationNameChange = (event) => {
+        locationName = event.target.value;
+        //console.log(locationName);
+    }
+
+
+    addData = (event) => {
+        console.log("werwerwe");
+        this.connections();
+
+
+    }
+
+    async connections() {
+        await this.addDataConnection();
+        await this.updateUIConnection();
+    }
+
+    async addDataConnection() {
+        let response = await fetch("http://localhost:5001/addUserData", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                location_name: locationName
+            })
+        })
+    }
+
+    async updateUIConnection() {
+        let response = await fetch("http://localhost:5001/getUserData", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+            })
+        })
+
+        let responseJson = await response.json();
+        this.setState({ score: responseJson.score });
+        this.updateLevelStyle(responseJson.score);
+        console.log(responseJson);
+
+    }
+
 
     render() {
         return (
             <div>
                 <DocumentMeta {...meta} />
                 {this.state.linkBar}
-                <h1>YOUR SCORE</h1>
-                <div className="bigText">2020</div>
+                <div className = "forceMargin">
+                <h1>EXPOSURE LEVEL</h1>
+                <div className={this.state.levelStyle} > { this.state.score }</div>
                 <div>
-                    <h1>THANK YOU FOR HELPING TO FIGHT COVID 19</h1>
+                    <h2>THANK YOU FOR HELPING TO FIGHT COVID 19</h2>
                     <div className="form">
-                        <TextInputComponent label="Where have you been? " />
-                        <ButtonComponent label="SUBMIT" />
+                        <TextInputComponent label="Where have you been? " logChange={this.locationNameChange}/>
+                        <ButtonComponent label="SUBMIT" isPressed={this.addData} />
+                        <h4> Suggested Locations </h4>
+                        {this.state.locationsUI}
 
+                    </div>
                     </div>
                 </div>
             </div>
